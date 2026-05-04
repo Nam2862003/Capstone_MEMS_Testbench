0d24473f965657602c37584876077cbf1ce87f8d
Nam2862003 <hoangnam2862003@gmail.com>
2026-05-04 10:58:22 +1000

    Remove unnessaries

--- Nucleo_H7S3L8/Appli/build/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "Nucleo_H7S3L8_Appli.map"
-  )
-endif()
--- Nucleo_H7S3L8/build/Debug/CMakeFiles/clean_additional.cmake
+++
@@ -1,8 +0,0 @@
-# Additional clean files
-cmake_minimum_required(VERSION 3.16)
-
-if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
-  file(REMOVE_RECURSE
-  "C:\\STM32\\Capstone_MEMS_Testbench\\Nucleo_H7S3L8\\Appli\\build"
-  )
-endif()
