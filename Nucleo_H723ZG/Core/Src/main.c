c1eed0fc35393c6d7fd97a9ed32df7d25d3c93af
Nam2862003 <hoangnam2862003@gmail.com>
2026-05-18 12:59:31 +1000

    Fix nucleo h723zg, PE14 is off by default

--- Nucleo_H723ZG/Core/Src/main.c
+++ Nucleo_H723ZG/Core/Src/main.c
@@ -679,10 +679,7 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7|GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_11
-                          |GPIO_PIN_13, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_14, GPIO_PIN_SET);
+                          |GPIO_PIN_13|GPIO_PIN_14, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
